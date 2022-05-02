b1aeba53e7626f44828dbad0ca5fa681fe176ff8
0xlax <harshu8310@gmail.com>
2022-05-02 13:19:53 +0530

    test and workflow setup

---
+++ account.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World!")
+}
+
+func Add(x, y int) (res int) {
+	return x + y
+}
---
+++ account_test.go
@@ -0,0 +1,12 @@
+package account
+
+import "testing"
+
+func TestAdd(t *testing.T) {
+	got := Add(4, 6)
+	want := 10
+
+	if got != want {
+		t.Errorf("got %q, wanted %q", got, want)
+	}
+}
